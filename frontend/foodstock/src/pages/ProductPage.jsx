42f3678c75f0f76b65733f564ffd7cbbb2c25d0d
Raden Rovcanin <radenrovcanin12@gmial.com>
2022-06-02 22:04:48 +0200

    basic info div is styled

--- frontend/foodstock/src/pages/ProductPage.jsx
+++ frontend/foodstock/src/pages/ProductPage.jsx
@@ -1,6 +1,7 @@
 import useGet from "../customHooks/useGet";
 import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
+import {BsCartPlus} from 'react-icons/bs'
 const ProductPage = () => {
 
     const {id} = useParams();
@@ -17,10 +18,13 @@ const ProductPage = () => {
                             <h2>{data[0].Name}</h2>
                             <p>{data[0].Description}</p>
                             <div className="price-and-btn">
-                                <h1>{data[0].Price}</h1>
-                                <button></button>
+                                <h1>{data[0].Price}&#x20AC;</h1>
+                                <button><BsCartPlus size={20}></BsCartPlus></button>
                             </div>
                         </div>
+                    </div>
+                    <div className="additional-info">
+
                     </div>
 
                 </div>
